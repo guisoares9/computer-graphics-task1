e47b4559fd36d46365190e6ef33f8136e0732004
Guilherme Soares <gsoares.silvestre@gmail.com>
2021-10-07 20:53:49 -0300

    transform.py commented

--- transform.py
+++ transform.py
@@ -1,9 +1,11 @@
 import numpy as np
 import math
 
+# Create a indentity matrix
 def createEyeMat():
     return np.eye(4)
 
+# Apply translation to the matrix
 def translate(tx, ty, mat_transform):
     mat_translation = np.array([[1.0, 0.0, 0.0, tx ], 
                                 [0.0, 1.0, 0.0, ty ], 
@@ -11,6 +13,7 @@ def translate(tx, ty, mat_transform):
                                 [0.0, 0.0, 0.0, 1.0]], np.float32)
     return np.dot(mat_translation, mat_transform) 
 
+# Apply scale to the matrix
 def scale(sx, sy, mat_transform):
     mat_scale = np.array([[sx, 0.0, 0.0, 0.0], 
                             [0.0, sy , 0.0, 0.0], 
@@ -18,6 +21,7 @@ def scale(sx, sy, mat_transform):
                             [0.0, 0.0, 0.0, 1.0]], np.float32)
     return np.dot(mat_scale, mat_transform) 
 
+# Apply rotation to the matrix
 def rotateZ(d, mat_transform):
     cos_d = math.cos(d)
     sin_d = math.sin(d)
